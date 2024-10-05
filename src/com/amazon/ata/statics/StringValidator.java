c2c3e8373b090b1b258c6c2da52cc0147ef01829
QuantumCodings <166426987+QuantumCodings@users.noreply.github.com>
2024-10-05 18:44:33 -0400

    Update StringValidator.java
    done

--- src/com/amazon/ata/statics/StringValidator.java
+++ src/com/amazon/ata/statics/StringValidator.java
@@ -2,26 +2,34 @@ package com.amazon.ata.statics;
 
 public final class StringValidator {
 
-    // Create a private constructor so StringValidator objects cannot be created.
+    
+    private static final String FORBIDDEN_WORD = "fish";
+    private static final int MAX_LENGTH = 250;
+
+    
     private StringValidator() {}
 
     /**
-     * Checks to see if the string entered contains a forbidden string.
+     * Checks to see if the string entered contains a forbidden string (e.g., "fish").
      *
      * @param input string that's being checked
      * @throws InvalidStringException if the input contains a forbidden string
      */
     public static void containsForbiddenWord(String input) throws InvalidStringException {
-        throw new InvalidStringException("The given String contains a forbidden word.");
+        if (input != null && input.toLowerCase().contains(FORBIDDEN_WORD.toLowerCase())) {
+            throw new InvalidStringException("The given String contains a forbidden word.");
+        }
     }
 
     /**
-     * Checks to see if the string entered has a length greater than the max allowed length.
+     * Checks to see if the string entered has a length greater than the max allowed length (250).
      *
      * @param input string that's being checked
-     * @throws InvalidStringException checks to see if the input is a forbidden string
+     * @throws InvalidStringException if the input exceeds the maximum allowed length
      */
     public static void greaterThanMaxLength(String input) throws InvalidStringException {
-        throw new InvalidStringException("The given String has a length greater than the max length allowed.");
+        if (input != null && input.length() > MAX_LENGTH) {
+            throw new InvalidStringException("The given String has a length greater than the max length allowed.");
+        }
     }
 }
